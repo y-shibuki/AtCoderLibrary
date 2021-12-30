53669cd6f34770e635131499bcf787bf124bb7d4
shibuki_373 <h-yoshida@toki.waseda.jp>
2021-12-30 22:50:05 +0900

    abc043/b

---
+++ abc043/b/main.py
@@ -0,0 +1,61 @@
+import sys
+
+"""
+よく使う関数
+bisect
+    bisect()
+collections
+    deque()
+    Counter()
+    most_common()
+    defaultdict()
+copy
+    deepcopy()
+    heapq
+    heapify()
+    heappush()
+    heappop()
+    itertools
+    accumulate()
+    groupby()
+    product()重複あり
+    permutations()重複なし(順列)
+    combinations()重複なし(組み合わせ)
+math
+    gcd()
+    ceil()
+    factorial()
+numpy
+    fmax()
+    argmax()
+    vstack()
+string
+    ascii_lowercase()
+    ascii_uppercase()
+    lower()
+    upper()
+    swapcase()
+"""
+
+# 整数を一行のみ読み込み
+# int
+def I(): return int(sys.stdin.readline().rstrip())
+# 空白区切りの整数を一行読み込み
+# list<int>
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+# 文字を一行読み込み
+# string
+def S(): return sys.stdin.readline().rstrip()
+# 空白区切りの文字を一行読み込み
+# list<string>
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+s = S()
+
+result = ""
+for k in s:
+    if k == "B":
+        result = result[:-1]
+    else:
+        result += k
+print(result)
