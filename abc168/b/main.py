a4e2a83e6b4bd069bf6af877d9f5fc7ea5655ea9
shibuki_373 <h-yoshida@toki.waseda.jp>
2021-12-30 23:33:38 +0900

    文字列

---
+++ abc168/b/main.py
@@ -0,0 +1,59 @@
+import sys
+
+"""
+よく使う関数
+bisect
+    bisect()
+collections
+    deque()
+    Counter()
+    most_common()
+    defaultdict()
+copy
+    deepcopy()
+    heapq
+    heapify()
+    heappush()
+    heappop()
+    itertools
+    accumulate()
+    groupby()
+    product()重複あり
+    permutations()重複なし(順列)
+    combinations()重複なし(組み合わせ)
+math
+    gcd()
+    ceil()
+    factorial()
+numpy
+    fmax()
+    argmax()
+    vstack()
+string
+    ascii_lowercase()
+    ascii_uppercase()
+    lower()
+    upper()
+    swapcase()
+"""
+
+# 整数を一行のみ読み込み
+# int
+def I(): return int(sys.stdin.readline().rstrip())
+# 空白区切りの整数を一行読み込み
+# list<int>
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+# 文字を一行読み込み
+# string
+def S(): return sys.stdin.readline().rstrip()
+# 空白区切りの文字を一行読み込み
+# list<string>
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+K = I()
+s = S()
+
+if len(s) > K:
+    print(s[:K] + "...")
+else:
+    print(s)
