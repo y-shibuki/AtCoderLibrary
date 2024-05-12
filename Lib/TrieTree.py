79b1c4777d70e3cf1f3882bfb9a0c83f5742c883
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2024-05-12 11:17:19 +0200

    Trie木の追加

---
+++ Lib/TrieTree.py
@@ -0,0 +1,17 @@
+# prefix tree
+# トライ木
+# 接頭辞系の文字列操作で使う
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
